d4efbb69d0f05f55c1110eb4fd9cd57a011ea9ba
Rohan Ahire <rohanahire006@gmail.com>
2025-11-02 01:39:15 +0530

    food partner model created

--- src/models/foodPartnerMoadel.js
+++ src/models/foodPartnerMoadel.js
@@ -1,1 +1,34 @@
 import mongoose from "mongoose";
+
+export const foodPartnerSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  contactName: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const foodPartnerModel = mongoose.model(
+  "foodpartner",
+  foodPartnerSchema
+);
