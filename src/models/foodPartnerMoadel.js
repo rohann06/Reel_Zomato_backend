41e9e7f6877ff272024d945562e3439e3f59989b
Rohan Ahire <rohanahire006@gmail.com>
2025-11-08 03:42:40 +0530

    Added the timestamp in food partner model

--- src/models/foodPartnerMoadel.js
+++ src/models/foodPartnerMoadel.js
@@ -1,32 +1,37 @@
 import mongoose from "mongoose";
 
-export const foodPartnerSchema = mongoose.Schema({
-  ownerName: {
-    type: String,
-    required: true,
+export const foodPartnerSchema = mongoose.Schema(
+  {
+    ownerName: {
+      type: String,
+      required: true,
+    },
+    restaurantName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    restaurantAddress: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  restaurantName: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  restaurantAddress: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const foodPartnerModel = mongoose.model(
   "foodPartner",
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -13,6 +13,7 @@ const userShema = new mongoose.Schema(
     },
     password: {
       type: String,
+      required: true,
     },
   },
   {
