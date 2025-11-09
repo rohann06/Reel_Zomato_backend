ef7477914f6b550b83a6392797c8b2da47e30a22
Rohan Ahire <rohanahire006@gmail.com>
2025-11-10 03:14:24 +0530

    Aadded the token code

--- src/controllers/auth/userAuthController.js
+++ src/controllers/auth/userAuthController.js
@@ -60,7 +60,12 @@ export const loginUser = async (req, res) => {
       },
       process.env.JWT_SECRET
     );
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true, // only true if using https (like render.com)
+      sameSite: "none", // 👈 required for cross-site cookies
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
     res.status(200).json({
       message: "users logged in successfully",
