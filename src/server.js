59384f767b94e91e8f49594c631c762c84374064
Rohan Ahire <rohanahire006@gmail.com>
2025-11-25 04:27:08 +0530

    Changed the domain for the cors

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const PORT = 5001;
 // 🧩 CORS CONFIG
 app.use(
   cors({
-    origin: "http://localhost:3000", // your frontend URLs
+    origin: "https://foodscroll06.vercel.app/", // your frontend URLs
     credentials: true,
   })
 );
